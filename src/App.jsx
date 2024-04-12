bbe4d08cc184e9c4ebe4ea32f2210939081e8251
daffaabp <daffa.budi2003@gmail.com>
2024-04-12 07:54:34 +0700

    1_event_handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const title = "Welcome to My App";
+  const age = 20;
+  const link = "daffa-portofolio.000webhostapp.com";
+
+  const clickMe = (name) => {
+    console.log('Hello' + name);
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Header />
+        <h1>{title}</h1>
+        <h1>{age * 2}</h1>
+        <a href={link}>Go To My Portofolio</a>
+        <button onClick={() => clickMe('Daffa')}>Click Me</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
