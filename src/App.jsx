b4cb64247ed144ac1869463d60f3796cf0bca2f4
daffaabp <daffa.budi2003@gmail.com>
2024-04-12 21:17:27 +0700

    7_fetchData

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import ProductList from "./components/ProductList";
 
 function App() {
-  const [products, setProducts] = useState([
-    { id: 1, title: "Product 1", price: 500 },
-    { id: 2, title: "Product 2", price: 1000 },
-    { id: 3, title: "Product 3", price: 1500 },
-    { id: 4, title: "Product 4", price: 2000 },
-    { id: 5, title: "Product 5", price: 2500 },
-    { id: 6, title: "Product 6", price: 3000 },
-  ]);
-
-  const deleteProduct = (productId) => {
-    const newProducts = products.filter(product => product.id !== productId);
-    setProducts(newProducts);
-  };
-
-  const [name, setName] = useState("Daffa");
-
-  useEffect(() => {
-    console.log("Use Effect running");
-  }, [name]);
-
   return (
-    <div>
+    <div className="container">
       <Router>
         <Routes>
-          <Route exact path="/" element={<ProductList products={products} deleteProduct={deleteProduct} />} />
+          <Route exact path="/" element={<ProductList />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,15 +1,43 @@
+import { useState, useEffect } from "react";
 
-const ProductList = ({ products, deleteProduct }) => {
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fectData();
+  }, []);
+
+  const fectData = async () => {
+    const response = await fetch("http://localhost:3000/products");
+    const data = await response.json();
+    setProducts(data);
+  };
+
   return (
     <div>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            {product.title} - {product.price}
-            <button onClick={() => deleteProduct(product.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      <table className="table is-stripped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product, index) => (
+            <tr key={product.id}>
+              <td>{index + 1}</td>
+              <td>{product.title}</td>
+              <td>{product.price}</td>
+              <td>
+                <button className="button is-small is-info">Edit</button>
+                <button className="button is-small is-danger">Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
